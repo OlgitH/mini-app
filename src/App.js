9234988d4f2449222e84965c94ae23aeec66b9e9
OlgitH <berman.oj@gmail.com>
2022-07-05 11:58:55 +0100

    ~

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+
 import './App.css';
 
 function App() {
+
+  const dataCars = [
+    {make: 'Volvo', model: 'v40'},
+    {make: 'Ferrari', model: 'f50'}
+  ]
+  const [newCar, setNewCar] = useState(null);
+
+  const [cars, setCars] = useState(dataCars);
+
+  const [bannerShowing, setBannerShowing] = useState(false);
+ 
+  useEffect(() => {
+    function showBanner(){
+      setBannerShowing(true)
+    }
+    setTimeout(showBanner, 1000)
+  },[])
+
+  useEffect(() => {
+    console.log(newCar);
+    console.log('newCar changed');
+  },[newCar])
+
+  useEffect(() => {
+    console.log(cars);
+    alert('newCar added');
+  },[cars])
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    // const updatedCars = cars.push({make: newCar, model: null});
+    setCars(cars => [...cars, newCar]);
+
+    // setCars(updatedCars);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1>Use Effect demo</h1>
+
+         {cars.map((car, i) => <p key={i}>{car.make} {car.model && car.model}</p>)}
+
+         <p>Lorem ipsum dolor sit amet, consectetur 
+          adipisicing elit. Illum necessitatibus dolorum 
+          optio quasi, minima distinctio, voluptate praesentium 
+          ea impedit fuga excepturi. Sunt natus ducimus adipisci 
+          quis, inventore veniam esse debitis.</p>
+
+          <form onSubmit={submitForm}>
+            <label htmlFor="addCar">Add new car</label>
+            <input 
+            type="text" 
+            id="addCar" 
+            onChange={
+              (e) => setNewCar({...newCar,  make: e.target.value, model: null })
+            }
+            />
+            <button type="submit">Submit</button>
+          </form>
+
+         
+          {
+            bannerShowing ?
+            <div className="cta-banner">
+              <span>Please sign up to the newsletter</span>
+              <button onClick={() => setBannerShowing(false)} >
+                X
+              </button>
+            </div>
+            :
+            ''
+          }
+          
     </div>
   );
 }
